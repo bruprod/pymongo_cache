cd4f37d1327138fc2a98b62af74dfe8b730bc47a
bruprod <andreas.bruchmann@gmx.net>
2024-01-18 21:48:14 +0100

    Creating collection once and removal of has_item for MongoDbBackend

--- cache_backend/mongodb_backend/MongoDBCacheBackend.py
+++ cache_backend/mongodb_backend/MongoDBCacheBackend.py
@@ -13,9 +13,12 @@ from cache_backend.QueryInfo import QueryInfo
 
 class MongoDBCacheBackend(CacheBackendBase):
     """ Class for caching MongoDB queries in a Mongodb database. """
+    _cache_collection = None
 
-    def __init__(self, collection: Collection, ttl: int = 0, max_item_size: int = 1 * 10 ** 6, max_num_items: int = 1000):
+    def __init__(self, collection: Collection, ttl: int = 0, max_item_size: int = 1 * 10 ** 6,
+                 max_num_items: int = 1000):
         super().__init__(collection, ttl, max_item_size, max_num_items)
+        self._cache_collection = self._get_cache_collection()
 
     def _get_cache_collection(self) -> Collection:
         """ Create the table if it doesn't exist. """
@@ -25,15 +28,9 @@ class MongoDBCacheBackend(CacheBackendBase):
         coll.create_indexes([IndexModel([(COLLECTION_NAME, ASCENDING), (HASH_VAL, ASCENDING)], name="CacheIndex")])
         return coll
 
-    def has_item(self, key: QueryInfo) -> bool:
-        """ Check if the cache has the item. """
-        coll = self._get_cache_collection()
-        res = coll.find_one({COLLECTION_NAME: self.collection.name, HASH_VAL: key.__hash__()})
-        return res is not None
-
     def get(self, key: QueryInfo) -> Any:
         """ Get the value from the cache. """
-        entry = self._get_cache_collection().find_one({COLLECTION_NAME: self.collection.name, HASH_VAL: key.__hash__()})
+        entry = self._cache_collection.find_one({COLLECTION_NAME: self.collection.name, HASH_VAL: key.__hash__()})
         if entry is not None:
             return entry[VALUE]
         return None
@@ -42,16 +39,17 @@ class MongoDBCacheBackend(CacheBackendBase):
         """ Set the value in the cache. """
 
         cache_entry = CacheEntry(key, value, self.collection.name, key.__hash__())
-        self._get_cache_collection().update_one({COLLECTION_NAME: self.collection.name, HASH_VAL: key.__hash__()},
-                                                {"$set": cache_entry.to_dict()}, upsert=True)
+        self._cache_collection.update_one({COLLECTION_NAME: self.collection.name, HASH_VAL: key.__hash__()},
+                                          {"$set": cache_entry.to_dict()}, upsert=True,
+                                          bypass_document_validation=True)
 
     def delete(self, key: QueryInfo) -> None:
         """ Delete the value from the cache. """
-        self._get_cache_collection().delete_one({COLLECTION_NAME: self.collection.name, HASH_VAL: key.__hash__()})
+        self._cache_collection.delete_one({COLLECTION_NAME: self.collection.name, HASH_VAL: key.__hash__()})
 
     def clear(self) -> None:
         """ Clear the cache. """
-        self._get_cache_collection().delete_many({COLLECTION_NAME: self.collection.name})
+        self._cache_collection.delete_many({COLLECTION_NAME: self.collection.name})
 
     def get_all(self) -> Dict[QueryInfo, Any]:
         """ Get all the values from the cache. """
